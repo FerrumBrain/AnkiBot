fd388e19f2f3d08f35a42b5ab6c76a8d7971faf8
Timur <Kudashev>
2021-12-13 03:47:57 +0300

    Fixed cancel quiz: not counting cancelled question

--- bot.py
+++ bot.py
@@ -210,6 +210,8 @@ def handle_finish_quiz(update: Update, callback_context: CallbackContext):
     right_answer = 0
     for index, result in enumerate(zip(callback_context.user_data["user_answers"],
                                        callback_context.user_data["questions"])):
+        if index == len(callback_context.user_data["user_answers"]) - 1 and callback_context.user_data["is_cancel"]:
+            break
         current_answer, current_right_answer = result
         current_right_answer = current_right_answer[0]
         if current_answer == current_right_answer:
